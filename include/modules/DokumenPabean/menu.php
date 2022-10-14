8215b5d7303f3f234456764e3cbd107541a3264b
muhammadamran <amranhakim9@gmail.com>
2022-10-15 03:13:26 +0700

    Initial commit

--- include/modules/DokumenPabean/menu.php
+++ include/modules/DokumenPabean/menu.php
@@ -20,27 +20,29 @@ if ($resultRoleModules['v_bc'] == 'show') {
         <span>BC</span>
     </a>
     <ul class="sub-menu">
-        <li class="<?= $uriSegments[1] == 'dp_bc2_3.php' || $uriSegments[1] == 'dp_bc2_7.php' ? 'active' : '' ?>" style="display: <?= $resultRoleModules['v_bc']; ?>">
-            <!-- <a href="dp_bc2_3.php">BC 2.3 / Master Data</a> -->
-            <a href="dp_bc2_7.php">BC 2.7 / Master Data</a>
+        <li class="<?= $uriSegments[1] == 'dp_bc2_3.php' ? 'active' : '' ?>"
+            style="display: <?= $resultRoleModules['v_bc']; ?>">
+            <a href="dp_bc2_3.php">BC 2.3</a>
         </li>
-        <!-- <li class="<?= $uriSegments[1] == 'dp_bc2_5.php' ? 'active' : '' ?>">
-            <a href="dp_bc2_5.php">BC 2.5</a>
+        <li class="<?= $uriSegments[1] == 'dp_bc2_5.php' ? 'active' : '' ?>"
+            style="display: <?= $resultRoleModules['v_bc']; ?>">
+            <a href="dp_bc2_5.php">BC 2.4</a>
         </li>
-        <li class="<?= $uriSegments[1] == 'dp_bc2_6_1.php' ? 'active' : '' ?>">
+        <li class="<?= $uriSegments[1] == 'dp_bc2_6_1.php' ? 'active' : '' ?>"
+            style="display: <?= $resultRoleModules['v_bc']; ?>">
             <a href="dp_bc2_6_1.php">BC 2.6.1</a>
         </li>
-        <li class="<?= $uriSegments[1] == 'dp_bc2_6_2.php' ? 'active' : '' ?>">
-            <a href="dp_bc2_6_2.php">BC 2.6.2</a>
-        </li>
-        <li class="<?= $uriSegments[1] == 'dp_bc2_7.php' ? 'active' : '' ?>">
+        <li class="<?= $uriSegments[1] == 'dp_bc2_7.php' ? 'active' : '' ?>"
+            style="display: <?= $resultRoleModules['v_bc']; ?>">
             <a href="dp_bc2_7.php">BC 2.7</a>
         </li>
-        <li class="<?= $uriSegments[1] == 'dp_bc4_0.php' ? 'active' : '' ?>">
+        <li class="<?= $uriSegments[1] == 'dp_bc4_0.php' ? 'active' : '' ?>"
+            style="display: <?= $resultRoleModules['v_bc']; ?>">
             <a href="dp_bc4_0.php">BC 4.0</a>
         </li>
-        <li class="<?= $uriSegments[1] == 'dp_bc4_1.php' ? 'active' : '' ?>">
+        <li class="<?= $uriSegments[1] == 'dp_bc4_1.php' ? 'active' : '' ?>"
+            style="display: <?= $resultRoleModules['v_bc']; ?>">
             <a href="dp_bc4_1.php">BC 4.1</a>
-        </li> -->
+        </li>
     </ul>
 </li>
