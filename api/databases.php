b9fad576e3527f8ed86fe44c77b6e92f22e1f29a
muhammadamran <amranhakim9@gmail.com>
2022-10-15 21:52:27 +0700

    Initial comiit

---
+++ api/databases.php
@@ -0,0 +1,25 @@
+<?php
+include "include/connection.php";
+
+$dataDB = $dbcon->query("SELECT * FROM api WHERE id=1 LIMIT 1", 0);
+$cek = $dataDB->num_rows;
+
+if ($cek > 0) {
+    $data = [];
+
+    while ($result = $dataDB->fetch_assoc()) {
+        $data[] = [
+            'data' => $result['data']
+        ];
+    }
+
+    echo json_encode([
+        'status' => 200,
+        'result' => $data
+    ]);
+} else {
+    echo json_encode([
+        'status' => 404,
+        'result' => 'Data not found'
+    ]);
+}
