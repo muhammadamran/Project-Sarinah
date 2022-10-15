326f25df02cbf42e46630b0637a7c7bd27ea356d
muhammadamran <amranhakim9@gmail.com>
2022-10-16 01:21:34 +0700

    Initial commit

--- include/api.php
+++ include/api.php
@@ -5,7 +5,7 @@ function get_content($URL)
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_URL, $URL);
-    // curl_setopt($ch, CURLOPT_PORT, 8091);
+    curl_setopt($ch, CURLOPT_PORT, 8091);
     $data = curl_exec($ch);
     curl_close($ch);
     return $data;
