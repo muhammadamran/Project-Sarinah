87d64a0116c16c2f9536b7223c6bfc18f412c9a0
muhammadamran <amranhakim9@gmail.com>
2022-10-16 09:48:33 +0700

    Initial commit

--- 00/182.php
+++ 00/182.php
@@ -5,14 +5,15 @@ function get_content($URL)
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_URL, $URL);
-    curl_setopt($ch, CURLOPT_PORT, 8091);
+    // curl_setopt($ch, CURLOPT_PORT, 8091);
+    curl_setopt($ch, CURLOPT_PROXY, "182.23.104.212:8091");
     $data = curl_exec($ch);
     curl_close($ch);
     return $data;
 }
 // API - 
-$content = get_content('http://182.23.104.212/tpbbackend/api/dataBC23.php');
+$content = get_content('http://182.23.104.212:8091/tpbbackend/api/dataBC23.php');
 $data = json_decode($content, true);
 
-var_dump($$data['status']);
+var_dump($data['status']);
 exit;
