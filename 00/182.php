d852ea3f12072694e339cba32b078fa443488cdf
muhammadamran <amranhakim9@gmail.com>
2022-10-16 09:52:27 +0700

    Initial commit

--- 00/182.php
+++ 00/182.php
@@ -3,10 +3,10 @@
 function get_content($URL)
 {
     $ch = curl_init();
+    curl_setopt($ch, CURLOPT_PROXY, "182.23.104.212:8091");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_URL, $URL);
     // curl_setopt($ch, CURLOPT_PORT, 8091);
-    curl_setopt($ch, CURLOPT_PROXY, "182.23.104.212:8091");
     $data = curl_exec($ch);
     curl_close($ch);
     return $data;
