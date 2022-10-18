6e46fc5e5f7310172f5680aadf1cb7e4144bd59e
muhammadamran <amranhakim9@gmail.com>
2022-10-18 14:42:12 +0700

    Intial commit

--- report_masuk_barang.php
+++ report_masuk_barang.php
@@ -63,44 +63,34 @@ $data = json_decode($content, true);
                 </div>
                 <div class="panel-body text-inverse">
                     <form action="" method="POST">
-                        <div class="modal-header">
-                            <h4 class="modal-title">[Laporan Masuk Barang] Filter Tanggal</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        <div class="row" style="display: grid;justify-content: center;align-items: center;">
+                            <div class="col-12" style="display: flex;justify-content: center;">
+                                <img src="assets/img/svg/realisasi_b.svg" alt="Laporan Realisasi Mitra Per Tahun"
+                                    class="image" width="50%">
+                            </div>
                         </div>
-                        <div class="modal-body">
-                            <div class="row" style="display: grid;justify-content: center;align-items: center;">
-                                <div class="col-12" style="display: flex;justify-content: center;">
-                                    <img src="assets/img/svg/realisasi_b.svg" alt="Laporan Realisasi Mitra Per Tahun"
-                                        class="image" width="50%">
+                        <hr>
+                        <div class="row" style="display: flex;align-items: center;">
+                            <div class="col-xl-4">
+                                <div class="form-group">
+                                    <input type="date" name="StartTanggal" class="form-control"
+                                        value="<?= $StartTanggal; ?>" required>
                                 </div>
                             </div>
-                            <hr>
-                            <div class="row" style="display: flex;align-items: center;">
-                                <div class="col-xl-5">
-                                    <div class="form-group">
-                                        <input type="date" name="StartTanggal" class="form-control"
-                                            value="<?= $StartTanggal; ?>" required>
-                                    </div>
+                            <div class="col-xl-2" style="display: flex;justify-content: center;">
+                                <div class="form-group">
+                                    s.d
                                 </div>
-                                <div class="col-xl-2" style="display: flex;justify-content: center;">
-                                    <div class="form-group">
-                                        s.d
-                                    </div>
-                                </div>
-                                <div class="col-xl-5">
-                                    <div class="form-group">
-                                        <input type="date" name="EndTanggal" class="form-control"
-                                            value="<?= $EndTanggal; ?>" required>
-                                    </div>
+                            </div>
+                            <div class="col-xl-4">
+                                <div class="form-group">
+                                    <input type="date" name="EndTanggal" class="form-control"
+                                        value="<?= $EndTanggal; ?>" required>
                                 </div>
                             </div>
                         </div>
-                        <div class="modal-footer">
-                            <a href="javascript:;" class="btn btn-white" data-dismiss="modal"><i
-                                    class="fas fa-times-circle"></i> Tutup</a>
-                            <button type="submit" name="filter_date" class="btn btn-default"><i
-                                    class="fas fa-filter"></i> Filter Tanggal</button>
-                        </div>
+                        <button type="submit" name="filter_date" class="btn btn-default"><i class="fas fa-filter"></i>
+                            Filter Tanggal</button>
                     </form>
                 </div>
             </div>
