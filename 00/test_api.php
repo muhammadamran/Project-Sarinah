6278229e4ac24267b6717a56ead1fb1638eda3b8
muhammadamran <amranhakim9@gmail.com>
2022-10-20 20:18:55 +0700

    Initial commit

--- 00/test_api.php
+++ 00/test_api.php
@@ -1,11 +1,11 @@
 <?php
 // API
-function get_content($URL, $PORT)
+function get_content($URL)
 {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_URL, $URL);
-    curl_setopt($ch, CURLOPT_PORT, $PORT);
+    curl_setopt($ch, CURLOPT_PORT, $_SERVER['SERVER_PORT']);
     $data = curl_exec($ch);
     curl_close($ch);
     return $data;
