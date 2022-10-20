b96684526cfd5acc095c3d50aaa3f0fa0f636781
muhammadamran <amranhakim9@gmail.com>
2022-10-20 20:20:15 +0700

    Initial commit

--- 00/test_api.php
+++ 00/test_api.php
@@ -5,14 +5,11 @@ function get_content($URL)
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_URL, $URL);
-    curl_setopt($ch, CURLOPT_PORT, $_SERVER['SERVER_PORT']);
+    curl_setopt($ch, CURLOPT_PORT, 8091);
     $data = curl_exec($ch);
     curl_close($ch);
     return $data;
 }
-
-var_dump($_SERVER['SERVER_PORT']);
-exit;
 
 // $content = get_content('http://117.54.102.106/tpbbackend/api/dataBC27.php');
 $content = get_content('http://api2.itinventory-sarinah.com:8091/tpbbackend/api/dataBC27.php');
