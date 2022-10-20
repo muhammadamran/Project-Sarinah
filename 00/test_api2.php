44660b47c9a8b0cb359e5d43b64582a7934a9caa
muhammadamran <amranhakim9@gmail.com>
2022-10-20 20:23:14 +0700

    Initial commit

---
+++ 00/test_api2.php
@@ -0,0 +1,13 @@
+<?php
+$url = "http://api2.itinventory-sarinah.com:8088/";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_TIMEOUT, 400);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, True);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+$report = curl_getinfo($ch);
+print_r($report);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result;
