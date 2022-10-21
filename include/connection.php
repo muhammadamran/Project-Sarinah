22899f2d09f878e9c412cb40a23f970c58c667cb
muhammadamran <amranhakim9@gmail.com>
2022-10-21 15:16:20 +0700

    Initial commit

--- include/connection.php
+++ include/connection.php
@@ -9,10 +9,3 @@ $dbcon = new mysqli($dbhost, $dbusername, $dbpassword, $dbname) or die(mysqli_co
 // QUERY SETTING API
 $dataAPI = $dbcon->query("SELECT * FROM api ORDER BY id ASC LIMIT 1");
 $resultAPI = mysqli_fetch_array($dataAPI);
-// QUERY SETTING APPLICATION
-$dataHeadSettting = $dbcon->query("SELECT * FROM tbl_setting");
-$resultHeadSetting = mysqli_fetch_array($dataHeadSettting);
-// QUERY SETTING REAL TIME
-$dataSetRealTime = $dbcon->query("SELECT * FROM tbl_realtime ORDER BY id DESC LIMIT 1");
-$resultSetRealTime = mysqli_fetch_array($dataSetRealTime);
-$SetTime = $resultSetRealTime['reload'];
